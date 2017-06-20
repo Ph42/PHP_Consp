fdcb7017d49d60bbcc9894e6c99521e8c943475d
Ph42 <metalcobalt@gmail.com>
2017-06-20 12:38:27 +0500

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,13 @@
+
+<html>
+<body>
+<h1>Yo!</h1>
+<?php
+$date = date('d.m y');
+$time = date('h:i:s');
+echo "Текущая дата: $date года <br>\n";
+echo "Текущее время: $time<br>\n";
+
+?>
+</body>
+</html>
